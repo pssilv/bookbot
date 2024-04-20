4e78ebf183a576323b066343ede85bbca48a72df
pssilv <85082308+pssilv@users.noreply.github.com>
2024-04-19 21:01:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ def main():
     num_words = get_num_words()
     num_chars = get_chars()
     num_letters = get_letters()
+    longest_word = get_longest_word()
     print(f"{"START":=^50}")
     print(f"WORD COUNTER:\n{num_words} words has been found in the document")
-    print(f"CHAR COUNTER:\n{num_letters} chars has been found in the document\nMOST COMMON CHARS:")
+    print(f"CHAR COUNTER:\n{num_letters} chars has been found in the document")
+    print(f"LONGEST WORD: {longest_word}\nMOST COMMON CHARS:")
     sorted_chars = get_sorted(num_chars)
     for dict in sorted_chars:
         char = dict["char"]
         num = dict["num"]
         print(f"{char} was found {num} times")
     print(f"{"END":=^50}")
-
 
 def get_book_text():
     book_path = "books/frankenstein.txt"
@@ -54,9 +55,32 @@ def get_letters():
         total_letters += 1
     return total_letters
 
-def reverse_book():
+def get_longest_word():
     text = get_book_text()
-    reversed_text = ""
-    reversed_text += text[-1:0:-1]
-    return reversed_text
+    words = text.split()
+    longest_word = ""
+    longest_word_counter = 0
+    for word in words:
+        if len(longest_word) < len(word):
+            longest_word = word
+    for word in words:
+        if longest_word.lower() == word.lower():
+            longest_word_counter += 1
+    return longest_word, f"The longest word appeareared: {longest_word_counter} time(s)"
+
+def search_word(*search_string):
+    text = get_book_text()
+    words = text.split()
+    string_counter = {}
+
+    for pos in search_string:
+        string_counter[pos.lower()] = 0
+
+    for key in string_counter:
+        for word in words:
+            if key == word.lower():
+                string_counter[key] += 1
+
+    return f"searched word dict: {string_counter}"
+
 main()
